ac5b3d1eeb2e8d7dcaff204d31be30dbef6f71a2
blackarmVR <honungsbjorn@protonmail.com>
2022-01-22 13:21:13 +0000

    improved build script

--- source/build.sh
+++ source/build.sh
@@ -1,5 +1,28 @@
+
+# eslint
+echo
+echo "check client.ts with airbnb eslint"
+if npx eslint client.ts
+then
+  echo "done"
+else
+  exit
+fi
+echo
+
+
+# transpile typescript
+echo "transpiling client.ts"
+if tsc
+then
+  echo "done"
+else
+  exit
+fi
+
 cat source.html > ../test.html
-cat client.js >> ../test.html
+grep -Ev 'use strict|ts-ignore' < client.js | sed 's/    /  /g' | sed 's/^/      /' >> ../test.html
 echo '    </script>' >> ../test.html
 echo '  </body>' >> ../test.html
 echo '</html>' >> ../test.html
+echo
--- source/client.js
+++ source/client.js
@@ -18,20 +18,29 @@ function onWindowResize() {
 function init() {
     container = document.createElement('div');
     document.body.appendChild(container);
+    // @ts-ignore
     scene = new THREE.Scene();
+    // @ts-ignore
     scene.background = new THREE.Color(0x444444);
+    // @ts-ignore
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10);
     camera.position.set(0, 1.6, 3);
+    // @ts-ignore
     controls = new OrbitControls(camera, container);
     controls.target.set(0, 1.6, 0);
     controls.update();
+    // @ts-ignore
     var floorGeometry = new THREE.PlaneGeometry(4, 4);
+    // @ts-ignore
     var floorMaterial = new THREE.MeshStandardMaterial({ color: 0x222222 });
+    // @ts-ignore
     var floor = new THREE.Mesh(floorGeometry, floorMaterial);
     floor.rotation.x = -Math.PI / 2;
     floor.receiveShadow = true;
     scene.add(floor);
+    // @ts-ignore
     scene.add(new THREE.HemisphereLight(0x808080, 0x606060));
+    // @ts-ignore
     var light = new THREE.DirectionalLight(0xffffff);
     light.position.set(0, 6, 0);
     light.castShadow = true;
@@ -41,21 +50,25 @@ function init() {
     light.shadow.camera.left = -2;
     light.shadow.mapSize.set(4096, 4096);
     scene.add(light);
-    //
+    // @ts-ignore
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
+    // @ts-ignore
     renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.shadowMap.enabled = true;
     renderer.xr.enabled = true;
     container.appendChild(renderer.domElement);
+    // @ts-ignore
     document.body.appendChild(VRButton.createButton(renderer));
     // controllers
     controller1 = renderer.xr.getController(0);
     scene.add(controller1);
     controller2 = renderer.xr.getController(1);
     scene.add(controller2);
+    // @ts-ignore
     var controllerModelFactory = new XRControllerModelFactory();
+    // @ts-ignore
     var handModelFactory = new XRHandModelFactory();
     // Hand 1
     controllerGrip1 = renderer.xr.getControllerGrip(0);
@@ -71,10 +84,13 @@ function init() {
     hand2 = renderer.xr.getHand(1);
     hand2.add(handModelFactory.createHandModel(hand2));
     scene.add(hand2);
-    //
+    // @ts-ignore
     var tempVector1 = new THREE.Vector3(0, 0, 0);
+    // @ts-ignore
     var tempVector2 = new THREE.Vector3(0, 0, -1);
+    // @ts-ignore
     var geometry = new THREE.BufferGeometry().setFromPoints([tempVector1, tempVector2]);
+    // @ts-ignore
     var line = new THREE.Line(geometry);
     line.name = 'line';
     line.scale.z = 5;
