67b84e4981bf04f26f4e8220d38441f19deb0ffd
blackarmVR <honungsbjorn@protonmail.com>
2022-01-25 17:06:59 +0000

    :)

--- imports.js
+++ imports.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 import { VRButton } from '../three.js/examples/jsm/webxr/VRButton.js';
 import { XRControllerModelFactory } from '../three.js/examples/jsm/webxr/XRControllerModelFactory.js';
 import { XRHandModelFactory } from '../three.js/examples/jsm/webxr/XRHandModelFactory.js';
